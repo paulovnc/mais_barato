add2163eef6cdc75130c36cf8dd189ac2a469db4
paulovnc <paulovnc@yahoo.com.br>
2013-11-07 00:29:56 -0200

    Modificação e criação de activities. Adição de figura e modificação do manifesto pra identificação de uma Activity.

---
+++ projeto/src/br/com/maisbarato/ComparacaoActivity.java
@@ -0,0 +1,12 @@
+package br.com.maisbarato;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ComparacaoActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.comparacao_activity);
+	}
+}
---
+++ projeto/src/br/com/maisbarato/MainActivity.java
@@ -0,0 +1,25 @@
+package br.com.maisbarato;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		ImageButton botaoLataCerveja = (ImageButton) findViewById(R.id.imagemLataCerveja);
+		botaoLataCerveja.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent it = new Intent("br.com.maisbarato.COMPARAR");
+				it.addCategory("br.com.maisbarato.AVALIACAO");
+				startActivity(it);
+			}
+		});
+	}
+}
