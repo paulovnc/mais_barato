8a97ce9a88a4b8b6deaf6ad4f221f36134764dc7
paulovnc <paulovnc@yahoo.com.br>
2013-11-20 00:42:16 -0200

    Alteração da tela de comparação de cervejas. Agora a tela dispões de radiogroups com pesos pre definidos.

--- projeto/src/br/com/maisbarato/ComparacaoActivity.java
+++ projeto/src/br/com/maisbarato/ComparacaoActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class ComparacaoActivity extends Activity {
@@ -14,23 +16,81 @@ public class ComparacaoActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.comparacao_activity);
+		//configurando eventos radiobutton da cerveja 1
+		RadioButton botaoOutroPrecoCerveja1 = (RadioButton) findViewById(R.id.radioButOutro1);
+		botaoOutroPrecoCerveja1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				EditText edtOutroPeso = (EditText) findViewById(R.id.editTextPesoEspecificoCerveja1);
+				edtOutroPeso.setVisibility(EditText.VISIBLE);
+			}
+		});
+		//configurando evendo radiobutton da cerveja 2
+		RadioButton botaoOutroPrecoCerveja2 = (RadioButton) findViewById(R.id.radioButOutro2);
+		RadioGroup group2 = (RadioGroup) findViewById(R.id.radioGroupPesoCerveja1);
+		
+		botaoOutroPrecoCerveja2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				EditText edtOutroPeso = (EditText) findViewById(R.id.editTextPesoEspecificoCerveja2);
+				edtOutroPeso.setVisibility(EditText.VISIBLE);
+			}
+		});
+		
 		Button botaoComparar = (Button) findViewById(R.id.buttonCalcular);
 		botaoComparar.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				try{
+					float precoCerveja1 = 0.0f;
+					float pesoCerveja1 = 0.0f;
+					float precoCerveja2 = 0.0f;
+					float pesoCerveja2 = 0.0f;
+					int escolha = 0;
+					
+					//tratando do peso e preco da primeira cerveja
 					EditText dados = (EditText) findViewById(R.id.precoCerveja1);
-					float precoCerveja1 = Float.parseFloat(dados.getText().toString());
-					dados = (EditText) findViewById(R.id.pesoCerveja1);
+					precoCerveja1 = Float.parseFloat(dados.getText().toString());
+					RadioGroup pesosCerveja1 = (RadioGroup) findViewById(R.id.radioGroupPesoCerveja1);
+					escolha = pesosCerveja1.getCheckedRadioButtonId();
+					switch(escolha){
+						case R.id.radioBut250:
+							pesoCerveja1 = 250.0f;
+							break;
+						case R.id.radioBut350:
+							pesoCerveja1 = 350.0f;
+							break;
+						case R.id.radioBut500:
+							pesoCerveja1 = 473.0f;
+							break;
+						case R.id.radioButOutro1:
+							dados = (EditText) findViewById(R.id.editTextPesoEspecificoCerveja1);
+							pesoCerveja1 = Float.parseFloat(dados.getText().toString());
+							break;
+					}
 					
-					float pesoCerveja1 = Float.parseFloat(dados.getText().toString());
+					//tratando do peso e preco da segunda cerveja
 					dados = (EditText) findViewById(R.id.precoCerveja2);
-					float precoCerveja2 = Float.parseFloat(dados.getText().toString());
-					dados = (EditText) findViewById(R.id.pesoCerveja2);
-					float pesoCerveja2 = Float.parseFloat(dados.getText().toString());
+					precoCerveja2 = Float.parseFloat(dados.getText().toString());
+					RadioGroup pesosCerveja2 = (RadioGroup) findViewById(R.id.radioGroupPesoCerveja2);
+					escolha = pesosCerveja2.getCheckedRadioButtonId();
+					switch(escolha){
+						case R.id.radioButSeg250:
+							pesoCerveja2 = 250.0f;
+							break;
+						case R.id.radioButSeg350:
+							pesoCerveja2 = 350.0f;
+							break;
+						case R.id.radioButSeg473:
+							pesoCerveja2 = 473.0f;
+							break;
+						case R.id.radioButOutro2:
+							dados = (EditText) findViewById(R.id.editTextPesoEspecificoCerveja1);
+							pesoCerveja2 = Float.parseFloat(dados.getText().toString());
+							break;
+					}
 					
-					
-					if(((int)pesoCerveja1 == 0) || ((int)pesoCerveja2 == 0) )
+					if(( Math.round(pesoCerveja1) == 0) || (Math.round(pesoCerveja2) == 0) )
 						throw new ArithmeticException();
 					if( (precoCerveja1/pesoCerveja1) < (precoCerveja2/pesoCerveja2) )
 						Toast.makeText(v.getContext(), "A primeira cerveja é a mais barata.", Toast.LENGTH_LONG).show();
